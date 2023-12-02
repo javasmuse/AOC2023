695cd21aae14b1b7b7eff09801cc4605d487bcf2
javasmuse <candacelewis@mac.com>
2023-12-02 21:57:25 +0000

    nested dictionary sample

--- day_two/day_two.py
+++ day_two/day_two.py
@@ -5,66 +5,105 @@
 # save the Game ID of possible 
 # sum Game ID return this
 
-# doesn't matter b/c the i will be the game id, so treat as list and combine  R B G 
-# save that in a new list with 0 as id
-# compare to possibles
+'''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''
 
+# RGB 12 13 14 
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+# input the csv to a nested dictionary
+# read in each as string 
+# - split at ; 
+# - regex digit before g (b r etc), digit b => b given ,, if all three correct , yes 
+
+given_bag = {
+    "red" : "12",
+    "green" : "13",
+    "blue" : "14"
 }
 
 games = {
-    "game1": {
-        "red": "4",
-        "blue": "3",
-        "green": "2",
-        "red": "1",
-        "green": "2",
-        "blue": "6"
+    "game1" : {
+        "pull1" : {
+            "red" : "4",
+            "blue" : "3",
+        },
+        "pull2" : {
+            "red" : "1",
+            "green" : "2",
+            "blue" : "6"
+        },
+        "pull3" : {
+            "green" : "2",
+        }
     },
-    "game2": {
-        "blue": "1",
-        "green": "2",
-        "green": "3",
-        "blue": "4",
-        "red": "1", 
-        "green": "1",
-        "blue": "1"
+    "game2" : {
+        "pull1" : {
+            "blue" : "1",
+            "green" : "2"
+        },
+        "pull2": {
+            "green" : "3",
+            "blue" : "4",
+            "red" : "1"
+        },
+        "pull3" : {
+            "green" : "1",
+            "blue" : "1"
+        }    
     },
     "game3": {
-        "green": "8",
-        "blue": "6",
-        "red": "20",
-        "blue": "5",
-        "red": "4",
-        "green": "13",
-        "green": "5",
-        "red": "1"
+        "pull1" : {
+            "green" : "8",
+            "blue" : "6",
+            "red" : "20"
+        },
+        "pull2" : {
+            "blue" : "5",
+            "red" : "4",
+            "green" : "13"
+        },
+        "pull3" : {
+            "green" : "5",
+            "red" : "1"
+        } 
     },
-    "game4": {
-        "green": "1", 
-        "red": "2", 
-        "blue": "6", 
-        "green": "3", 
-        "red": "6", 
-        "green": "3", 
-        "blue": "15", 
-        "red": "14"
-
+    "game4" : {
+        "pull1": {
+            "green" : "1", 
+            "red" : "2", 
+            "blue" : "6"
+        },
+        "pull2" : {
+            "green" : "3", 
+            "red" : "6"
+        },
+        "pull3" : {
+            "green" : "3", 
+            "blue" : "15", 
+            "red" : "14"
+        } 
     },
-    "game5": {
-        "red": "6", 
-        "blue": "1", 
-        "green": "3", 
-        "blue": "2", 
-        "red": "1", 
-        "green": "2"
+    "game5" : {
+        "pull1" : {
+            "red" : "6", 
+            "blue" : "1", 
+            "green" : "3"
+
+        },
+        "pull2" : {
+            "blue" : "2", 
+            "red" : "1", 
+            "green" : "2"
+        } 
     }
-    
     }
 
 
-print (games)
+print (games.values)
+
+
+
+# create a list and name the list the game number
