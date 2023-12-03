e7084b997a281ab0fc5d0b0a3118e354792cf588
javasmuse <candacelewis@mac.com>
2023-12-03 13:44:50 +0000

    working part 2

--- day_one/advent_of_code_day_oneP2.py
+++ day_one/advent_of_code_day_oneP2.py
@@ -19,11 +19,12 @@ def main():
 def pull_groups(set_here):
     for sets in set_here:
         word_digit = []
-        for i in range(len(sets)):
-            if sets[i].isdigit():
-                word_digit.append(sets[i])
+        x = sets.lower().replace("one", "1").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight", "8").replace("nine", "nine").replace("zero","0").replace("on", "1").replace("tw", "2").replace("thre", "3").replace("fiv", "5").replace("seve", "7").replace("eigh", "8").replace("nin", "9").replace("on", "1").replace("wo", "2").replace("hree", "3").replace("ight", "8").replace("ine", "9")
+        for i in range(len(x)):
+            if x[i].isdigit():
+                word_digit.append(x[i])
         y = int(word_digit[0] + word_digit[-1])
-        charlie_hold_numbs.append(y)
+        charlie_hold_numbs.append(y)       
 
 def totes_charles(x):
     y = 0
